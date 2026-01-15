8b0decce2b5554b325ca4f645d700e01114d5bae
Segasonics <114384404+Segasonics@users.noreply.github.com>
2026-01-15 15:30:47 +0530

    changed the instance

--- frontend/eBook-Creator/src/utils/axiosInstance.js
+++ frontend/eBook-Creator/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000/api/v1"
-      : "/api/v1",
+      : "https://ebook-creator-oxx7.onrender.com/api/v1",
   timeout: 80000,
   headers: {
     "Content-Type": "application/json",
